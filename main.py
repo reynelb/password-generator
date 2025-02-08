78b2eda9c9f312a87e53f67706895b64ea5f8044
Santiago B. <71462611+reynelb@users.noreply.github.com>
2025-02-09 01:32:21 +0300

    solved issue - main.py
    The two critical vulnerabilities mentioned have been fixed: https://github.com/reynelb/password-generator/issues/2#issue-2840255347

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ from constants import random_words, random_symbols
 def random_year():
     return random.randint(1100, 2099)
 
+def do_capitalize(word):
+    x = random.randint(0,1)
+    if x == 0:
+        return word.capitalize()
+    elif x == 1:
+        return word
+
 
 def pass_generator(custom_word, words_amount):
     password = ""
     random_words_to_use = random.sample(random_words, words_amount-1)
     words_to_use = [custom_word] + random_words_to_use
-    capitalised_word_pos = random.randint(0, words_amount-1)        
+    symbols_nums_to_use = random.sample(random_symbols, words_amount-1) + [str(random_year())]     
     random.shuffle(words_to_use)
+    random.shuffle(symbols_nums_to_use)
 
     for idx, word in enumerate(words_to_use):
-        current_word = word
-        if idx == capitalised_word_pos:
-            current_word = word.capitalize() + random.choice(random_symbols)
-        password += current_word
+        password = password + do_capitalize(word) + str(symbols_nums_to_use.pop(0))
     
-    password += str(random_year())
     return password
 
 
-test_password = pass_generator("firstTry", 2)
+test_password = pass_generator("first", 2)
 print(test_password) 
